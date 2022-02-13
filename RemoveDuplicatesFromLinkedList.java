4ce510c61b1d9c8b3b3bdc0783df9104012269f7
Boh <42015988+boh-prog@users.noreply.github.com>
2022-02-13 11:54:44 -0500

    Add files via upload

--- RemoveDuplicatesFromLinkedList.java
+++ RemoveDuplicatesFromLinkedList.java
@@ -1,57 +1,42 @@
-public class RemoveDuplicatesFromLinkedList
+/**
+ * Problem:
+ * Given the head of a sorted linked list, delete all duplicates such that each element appears only once.
+ * Return the linked list sorted as well.
+ *
+ * Input: head = [1,1,2]
+ * Output: [1,2]
+ *
+ * Input: head = [1,1,2,3,3]
+ * Output: [1,2,3]
+**/
+// * Definition for singly-linked list.
+class ListNode
 {
-//     * Definition for singly-linked list.
-      public class ListNode
-    {
-          int val;
-          ListNode next;
-          ListNode() {}
-          ListNode(int val) { this.val = val; }
-          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 
-    class Solution
+class Solution
+{
+    public ListNode deleteDuplicates(ListNode head)
     {
-        public ListNode deleteDuplicates(ListNode head)
+
+        ListNode curr = head;
+        //while there is a next value in List
+        while(curr != null)
         {
-            if(head == null) return head;
-
-            ListNode dummy = new ListNode();
-            ListNode curr = dummy;
-
-            while(head != null)
+            //compare current val with value in next node
+            while((curr.next != null) && (curr.val == curr.next.val))
             {
-                int val = head.val;
-                try
-                {
-                    if (head.next != null)
-                    {
-                        int adjVal = head.next.val;
-
-                        if(val != adjVal)
-                        {
-                            curr.val = val; curr.next = new ListNode(adjVal);
-
-                            curr = curr.next; head = head.next;
-                        }
-                        else if (val == adjVal) head = head.next;
-                    }
-                }
-                catch(NullPointerException e) //if head.next is null
-                {
-                    head = null; //head.next is null
-                }
-
-                // if(val != adjVal)
-                // {
-                //     curr = ListNode(val, adjVal);
-                //     curr = crr.next; head = head.next;
-                // }
-                // else if (val == adjVal) head = head.next;
-
+                //move curr's pointer to next next node
+                curr.next = curr.next.next;
             }
-
-            return dummy;
+            //move curr to next node
+            curr = curr.next;
         }
+        return head;
     }
 }
